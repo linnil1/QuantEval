d96ae45316df4b26df5c35e446baaf069b7a7d69
linnil1 <linnil1.886@gmail.com>
2020-08-29 12:12:26 +0800

    Make script cleaner

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -3,6 +3,7 @@ import re
 from Bio import SeqIO
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 
 def mRNAFilter(basedir, refdir, simdir, shortest_length):
@@ -248,7 +249,61 @@ def fasta_length_filter(file_fasta, file_result, threshold):
                 print(seq.format("fasta"), file=result, end="")
 
 
-def average_tpm(files_abundance, output):
+def readQuantTsv(method, path):
+    """
+    Read quantification result tsv file
+
+    Parameters
+    ----------
+    method: {kallisto, rsem, salmon}
+        The quantification tool
+    path: str
+        The path to tsv file
+
+    Returns
+    -------
+    data: pandas.Dataframe
+        The data frame has three column:
+            'name', 'tpm', 'count'
+    """
+
+    # read kallisto
+    if method == "kallisto":
+        data = pd.read_table(path, sep="\t")
+        data = data.rename(columns={'target_id': "name",
+                                    'tpm': "tpm",
+                                    'est_counts': "count"})
+        return data.loc[:, ("name", "tpm", "count")]
+
+    # read rsem
+    elif method == "rsem":
+        data = pd.read_table(path, sep="\t")
+        data = data.rename(columns={'transcript_id': "name",
+                                    'TPM': "tpm",
+                                    'expected_count': "count"})
+        return data.loc[:, ("name", "tpm", "count")]
+
+    # read salmon
+    elif method == "salmon":
+        data = pd.read_table(path, sep="\t")
+        data = data.rename(columns={'Name': "name",
+                                    'TPM': "tpm",
+                                    'NumReads': "count"})
+        return data.loc[:, ("name", "tpm", "count")]
+
+    # read answer
+    elif method == "answer":
+        data = pd.read_table(path, sep="\t")
+        data = data.rename(columns={'name': "name",
+                                    'answer_tpm': "tpm",
+                                    'answer_count': "count"})
+        return data.loc[:, ("name", "tpm", "count")]
+
+    else:
+        raise ValueError("Unknown Method")
+
+
+def average_tpm(files_abundance, file_output):
     """
     Average the tmp created by kallisto, rsem and salmon
 
@@ -263,46 +318,20 @@ def average_tpm(files_abundance, output):
     output: str
         The path you store the average value. Format: tsv.
     """
-    data = {}
-    for method, path in files_abundance.items():
-        # read kallisto
-        if method == "kallisto":
-            kallisto = pd.read_table(path, sep="\t")
-            kallisto = kallisto.rename(columns={'target_id': "name",
-                                                'tpm': "kallisto_tpm",
-                                                'est_counts': "kallisto_count"})
-            data['kallisto'] = kallisto.loc[:, ("name", "kallisto_tpm", "kallisto_count")]
+    # read all tables
+    tables = [readQuantTsv(method, path) for method, path in files_abundance.items()]
 
-        # read rsem
-        elif method == "rsem":
-            rsem = pd.read_table(path, sep="\t")
-            rsem = rsem.rename(columns={'transcript_id': "name",
-                                        'TPM': "rsem_tpm",
-                                        'expected_count': "rsem_count"})
-            data['rsem'] = rsem.loc[:, ("name", "rsem_tpm", "rsem_count")]
-
-        # read salmon
-        elif method == "salmon":
-            salmon = pd.read_table(path, sep="\t")
-            salmon = salmon.rename(columns={'Name': "name",
-                                            'TPM': "salmon_tpm",
-                                            'NumReads': "salmon_count"})
-            data['salmon'] = salmon.loc[:, ("name", "salmon_tpm", "salmon_count")]
-
-    # Only reserve the transcripts that got by all three methods
-    tables = list(data.values())
-    tmp = tables[0]
-    for d in tables[1:]:
-        tmp = pd.merge(tmp, d, on="name", how="inner")
-
-    # average
-    tmp['answer_tpm']   = np.mean([tmp[f'{method}_tpm']   for method in data.keys()], axis=0)
-    tmp['answer_count'] = np.mean([tmp[f'{method}_count'] for method in data.keys()], axis=0)
+    # Average the inner join
+    # -> only reserve the transcripts that got by all three methods
+    tmp = pd.concat(tables).groupby("name")
+    expression_table = tmp.mean().loc[tmp.count()['tpm'] == len(tables), :]
 
     # save it
-    expression_table = tmp.loc[:, ("name", "answer_tpm", "answer_count")]
+    expression_table.reset_index(level=0, inplace=True)
+    expression_table = expression_table.rename(columns={'tpm': "answer_tpm",
+                                                        'count': "answer_count"})
     expression_table = expression_table.round({'answer_tpm': 3, 'answer_count': 3})
-    expression_table.to_csv(output, sep="\t", index=False)
+    expression_table.to_csv(file_output, sep="\t", index=False)
 
 
 def readsCount(files_reads):
@@ -320,14 +349,11 @@ def readsCount(files_reads):
         The table that have two colnums: name and counts
     """
     # count sequence ID in fastq
-    count = {}
+    count = defaultdict(int)
     for f in files_reads:
         for seq in SeqIO.parse(f, 'fastq'):
             name = re.match('\S+?:\S+?:\S+?:(\S+?):', seq.id).group(1)
-            if name in count:
-                count[name] += 1
-            else:
-                count[name] = 1
+            count[name] += 1
 
     # store in table
     read_count = pd.DataFrame.from_dict(count, orient='index')
@@ -352,15 +378,13 @@ def readLib(file_lib):
         key: length
         value: counts
     """
-    count_dict = {}
+    count_dict = defaultdict(int)
     for lib_in in open(file_lib):
         data = lib_in.rstrip().split()
         start, end, count = int(data[0]), int(data[1]), int(data[-1])
         length = abs(start - end)
-        if length in count_dict:
-            count_dict[length] += count
-        else:
-            count_dict[length] = count
+        count_dict[length] += count
+
     return count_dict
 
 
